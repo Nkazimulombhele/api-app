aceef05c06b71a4319f02fd47bb1aa6169acd5e3
Nkazimulo Mbhele <nkazimulombhele74@gmail.com>
2023-01-27 14:58:01 +0200

    Api Project with React

---
+++ src/Department.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";  
+
+export class Department extends Component{
+
+    render(){
+        return (
+
+            <div>
+                <h3>This is Department page</h3>
+
+            </div>
+
+        )
+    }
+}
---
+++ src/Employee.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";  
+
+export class Employee extends Component{
+
+    render(){
+        return (
+
+            <div>
+                <h3>This is Employee page</h3>
+
+            </div>
+
+        )
+    }
+}
